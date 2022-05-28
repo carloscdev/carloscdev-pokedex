2222b11b763872abd5f3333e00fab5c75b4f656f
Carlos Córdova <ccordova.zosma@gmail.com>
2022-05-28 00:05:02 -0500

    load more

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://pokeapi.co/api/v2'
-export const PER_PAGE = 20
+export const PER_PAGE = 100
 export const POKEMON_TOTAL = 1126
 export const POKEMON_OBJECT = {
   id: '',
